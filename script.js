5fca5084f355f35fcf8de57bea4b7b379ce6399b
Arman__Yhyaa <111435138+Amankii@users.noreply.github.com>
2023-09-05 23:03:12 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,17 +3,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const btn0 = document.querySelector('#on');
     const btn1 = document.querySelector('#off');
     const img = document.querySelector('img');
- 
+    const click = new Audio('./images/clickm.mp3');
+
     btn0.onclick = () => {
 
         img.src='./images/bulb1zc.png';
-
+        click.play()
     };
 
     btn1.onclick = () => {
 
         img.src='./images/bulb0.png';
-
+        click.play()
     };
     
 
